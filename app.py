1ffe01a7801eed10e305436eac72a82600a929a8
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-09-16 20:59:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,5 @@ from config.attributes import *
 driver=webdriver.Chrome()
 driver.get(webiste)
 current_word_wrapper = driver.find_element_by_xpath('//*[@id="words"]/div[1]')
+
+print(current_word_wrapper)
